83b17d8e07b995df2d487a2ac443c4190ad5fed4
Alonso181006 <alonsomartin181006@gmail.com>
2022-09-21 11:32:32 -0600

    Update sketch.js

--- cheesboard/sketch.js
+++ cheesboard/sketch.js
@@ -6,10 +6,30 @@
 // - describe what you did to take this project "above and beyond"
 
 
+let i = 0;
+let h = 420;
+let w = h;
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(h, w);
 }
 
+let colorList = ["white", "black"];
+
 function draw() {
-  background(220);
+  for (let x = 0; x < width; x += width / 8) {
+    checker();
+    for (let y = 0; y < height; y += height / 8) {
+      square(x, y, width / 8);
+      checker();
+    }
+  }
+}
+function checker(){
+  if (i===0){
+    fill("black")
+    i++;
+  } else {
+    fill("white")
+    i--
+  }
 }
