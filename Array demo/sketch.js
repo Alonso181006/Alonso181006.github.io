42cab848be5d7f5c81d67c31591d9b0dbf20c196
Alonso181006 <alonsomartin181006@gmail.com>
2022-10-18 12:16:45 -0600

    Arrays

--- Array demo/sketch.js
+++ Array demo/sketch.js
@@ -1,15 +1,38 @@
-// Project Title
-// Your Name
+// Array Demo
+// Alonso Bastidas
 // Date
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let thisCircle = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  displayCircles();
 }
 
 function draw() {
-  background(220);
+  background(0);
+}
+
+function displayCircles(){
+  for (let i = 0; i < thisCircle.length; i++){
+    fill(thisCircle[i].theColor);
+    circle(thisCircle[i].x, thisCircle[i].y, thisCircle[i].radius);
+  }
+}
+
+function mousePressed(){
+  spawnCircle();
+}
+
+function spawnCircle(){
+  let theCircles = {
+    X: mouseX,
+    y: mouseY,
+    radius: 50,
+    theColor: color(random(255), random(255), random(255))
+  };
+  thisCircle.push(theCircles);
 }
