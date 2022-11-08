6ba97f5acd1f0c883eac1401bc73417c6d34ab54
Alonso181006 <alonsomartin181006@gmail.com>
2022-11-07 22:03:58 -0600

    have tanks displayed and moving

--- 2d Array Assigment/sketch.js
+++ 2d Array Assigment/sketch.js
@@ -5,254 +5,141 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-const ROWS = 8;
-const COLS = 16;
-let grid;
-let cellWidth;
-let cellHeight;
-let autoPlay = false;
-let playerX = 0;
-let playerY = 0;
-let entityX = 10;
-let entityY = 0;
-let neighbours;
-let Player, Entity, EntitySeen;
+let tiles;
+let player, enemy, madEnemy;
+let tilesHigh, tilesWide;
+let tileWidth, tileHeight;
 let levelToLoad;
 let lines;
+let playerX = 2;
+let playerY = 7;
 
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-  cellWidth = width/COLS;
-  cellHeight = height/ROWS;
-  grid = createEmpty2dArray(COLS, ROWS);
-  for (let y = 0; y < COLS; y++) {
-    for (let x = 0; x < ROWS; x++) {
-      let tileType = lines[y][x];
-      grid[y][x] = tileType;
-    }
-  }
-}
 
 function preload() {
   //load level data
-  levelToLoad = "assets/levels/0.txt";
+  levelToLoad = "0.txt";
   lines = loadStrings(levelToLoad);
 
 
   //load tile images
-  Entity = loadImage("tankBeige.png");
-  Player = loadImage("tankBlue.png");
-  EntitySeen = loadImage("tankRed.png");
+  player = loadImage("tankBlue.png");
+  enemy = loadImage("tankBeige.png");
+  madEnemy = loadImage("tankRed.png");
+  tiles[playerX][playerY] = "P";
 
+}
+
+function setup() {
+  // keep this a 4:3 ratio, or it will stretch in weird ways
+  createCanvas(windowWidth, windowHeight);
+
+  tilesHigh = lines.length;
+  tilesWide = lines[0].length;
+
+  tileWidth = width / tilesWide;
+  tileHeight = height / tilesHigh;
+
+  tiles = createEmpty2dArray(tilesWide, tilesHigh);
+
+  //put values into 2d array of characters
+  for (let y = 0; y < tilesHigh; y++) {
+    for (let x = 0; x < tilesWide; x++) {
+      let tileType = lines[y][x];
+      tiles[y][x] = tileType;
+    }
+  }
 }
 
 function draw() {
   display();
+
 }
 
-function display(){  
-  for (let y = 0; y < COLS; y++) {
-    for (let x = 0; x < ROWS; x++) {
-      showTile(grid[y][x], x, y);
+function display() {
+
+  for (let y = 0; y < tilesHigh; y++) {
+    for (let x = 0; x < tilesWide; x++) {
+      showTile(tiles[y][x], x, y);
     }
   }
 }
 
 function showTile(location, x, y) {
-  if (location === ".") {
-    rect( x*cellWidth, y*cellHeight, cellWidth, cellHeight);
+  if (location === "P") {
+    image(player, x * tileWidth, y * tileHeight, tileWidth, tileHeight);
   }
   else if (location === "E") {
-    image(Entity, x*cellWidth, y*cellHeight, cellWidth, cellHeight);
+    image(enemy, x * tileWidth, y * tileHeight, tileWidth, tileHeight);
   }
-  else if (location === "P") {
-    image(Player, x*cellWidth, y*cellHeight, cellWidth, cellHeight);
+  else if (location === "M") {
+    image(madEnemy, x * tileWidth, y * tileHeight, tileWidth, tileHeight);
   }
+  else if (location === ".") {
+    rect( x * tileWidth, y * tileHeight, tileWidth, tileHeight);
+  }
+
 }
 
-// function displayGrid(grid) {
-//   for (let y=0; y<ROWS; y++) {
-//     for (let x=0; x<COLS; x++) {
-//       if (grid[y][x] === 0) {
-//         fill("white");
-//       }
-//       else if (grid[y][x] === 1) {
-//         fill("black");
-//       }
-//       else if (grid[y][x] === 3) {
-//         fill("blue");
-//       }
-//       else if (grid[y][x] === 4) {
-//         fill("gray");
-//       }
-//       else if (grid[y][x] === 5) {
-//         fill("red");
-//       }
-//       rect(x*cellWidth, y*cellHeight, cellWidth, cellHeight);
-//     }   
-//   }
-// }
-
-// function checkAround(){
-//   let nextTurn = create2dArray(COLS, ROWS);
-
-//   for (let y=0; y<ROWS; y++) {
-//     for (let x=0; x<COLS; x++) {
-//       neighbours = 0;
-
-//       //look at all cells around this one...
-//       for (let i=-1; i<=1; i++) {
-//         for (let j=-1; j<=1; j++) {
-//           //edge case check
-//           if (y+i >= 1 && y+i < ROWS && x+j >= 1 && x+j < COLS) {
-//             if (grid[y+i][x+j] > 0){
-//               neighbours += 1;
-//             }
-//           }
-//         }
-//       }
-
-//       //don't count self!
-//       neighbours -= grid[y][x];
-
-//       //apply rules
-//       if (grid[y][x] === 3) { 
-//         if (neighbours === 1) {
-//           grid[y][x] = 4;
-//         }
-//         else {
-//           grid[y][x] = 3;
-//         }
-//       }
-
-//       if (grid[y][x] === 4) { 
-//         if (neighbours === 0) {
-//           grid[y][x] = 3;
-//         }
-//         else {
-//           grid[y][x] = 4;
-//         }
-//       }
-
-//       if (grid[y][x] === 4) {
-//         if (neighbours === 1) {
-//           grid[y][x]= 5;
-//         }
-//         else {
-//           grid[y][x] = 4;
-//         }
-//       }
-
-//       if (grid[y][x] === 5) { 
-//         if (neighbours === 0) {
-//           grid[y][x] = 4;
-//         }
-//         else {
-//           grid[y][x] = 4;
-//         }
-//       }
-//     }
-//   }
-//   displayGrid(grid);
-// }
-
-
-
-// function keyPressed() {
-//   if (keyCode === RIGHT_ARROW) {
-//     if (grid[playerY][playerX+1] === 0) {
-//       //reset old location to white
-//       grid[playerY][playerX] = 0;
-      
-//       //move
-//       playerX++;
-
-//       //set new player location
-//       grid[playerY][playerX] = 3;
-//     }
-//   }
-
-//   if (keyCode === LEFT_ARROW) {
-//     if (grid[playerY][playerX-1] === 0) {
-//       //reset old location to white
-//       grid[playerY][playerX] = 0;
-      
-//       //move
-//       playerX--;
-
-//       //set new player location
-//       grid[playerY][playerX] = 3;
-//     }
-//   }
-
-//   if (keyCode === UP_ARROW) {
-//     if (grid[playerY-1][playerX] === 0) {
-//       //reset old location to white
-//       grid[playerY][playerX] = 0;
-      
-//       //move
-//       playerY--;
-
-//       //set new player location
-//       grid[playerY][playerX] = 3;
-//     }
-//   }
-
-
-//   if (keyCode === DOWN_ARROW) {
-//     if (grid[playerY+1][playerX] === 0) {
-//       //reset old location to white
-//       grid[playerY][playerX] = 0;
-      
-//       //move
-//       playerY++;
-
-//       //set new player location
-//       grid[playerY][playerX] = 3;
-//     }
-//   }
-//   if(key === "a"){
-//     autoPlay = !autoPlay;
-//   }
-//   if (key === " ") {
-//     grid = checkAround(grid);
-//   }
-// }
-
-function createEmpty2dArray(COLS, ROWS) {
+function createEmpty2dArray(cols, rows) {
   let randomGrid = [];
-  for (let y = 0; y < ROWS; y++) {
+  for (let y = 0; y < rows; y++) {
     randomGrid.push([]);
-    for (let x = 0; x < COLS; x++) {
+    for (let x = 0; x < cols; x++) {
       randomGrid[y].push(0);
     }
   }
   return randomGrid;
 }
+function keyPressed() {
+  if (keyCode === RIGHT_ARROW) {
+    if (tiles[playerY][playerX+1] === ".") {
+      //reset old location to white
+      tiles[playerY][playerX] = ".";
+      
+      //move
+      playerX++;
 
-// function createRandom2dArray(COLS, ROWS) {
-//   let emptyArray = [];
-//   for (let y=0; y<ROWS; y++) {
-//     emptyArray.push([]);
-//     for (let x=0; x<COLS; x++) {
-//       if (random(100) < 50) {
-//         emptyArray[y].push(0);
-//       }
-//       else {
-//         emptyArray[y].push(1);
-//       }
-//     }
-//   }
-//   return emptyArray;
-// }
-// function mousePressed() {
-//   let xPos = Math.floor(mouseX/cellWidth);
-//   let yPos = Math.floor(mouseY/cellHeight);
+      //set new player location
+      tiles[playerY][playerX] = "P";
+    }
+  }
 
-//   if (grid[yPos][xPos] === 0) {
-//     grid[yPos][xPos] = 1;
-//   }
-//   else if (grid[yPos][xPos] === 1) {
-//     grid[yPos][xPos] = 0;
-//   }
-// }
+  if (keyCode === LEFT_ARROW) {
+    if (tiles[playerY][playerX-1] === ".") {
+      //reset old location to white
+      tiles[playerY][playerX] = ".";
+      
+      //move
+      playerX--;
+
+      //set new player location
+      tiles[playerY][playerX] = "P";
+    }
+  }
+
+  if (keyCode === UP_ARROW) {
+    if (tiles[playerY-1][playerX] === ".") {
+      //reset old location to white
+      tiles[playerY][playerX] = ".";
+      
+      //move
+      playerY--;
+
+      //set new player location
+      tiles[playerY][playerX] = "P";
+    }
+  }
+
+  if (keyCode === DOWN_ARROW) {
+    if (tiles[playerY+1][playerX] === ".") {
+      //reset old location to white
+      tiles[playerY][playerX] = ".";
+      
+      //move
+      playerY++;
+
+      //set new player location
+      tiles[playerY][playerX] = "P";
+    }
+  }
+}
