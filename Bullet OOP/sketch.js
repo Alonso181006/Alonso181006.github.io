c7a4cc6bdd5f5cb2143b600cbcb558cf543ebf98
Alonso181006 <alonsomartin181006@gmail.com>
2022-11-17 11:59:50 -0600

    Adding images

--- Bullet OOP/sketch.js
+++ Bullet OOP/sketch.js
@@ -6,44 +6,52 @@
 // - describe what you did to take this project "above and beyond"
 
 class Bullet{
-  constructor(x,y){
-    this.x = x;
-    this.y = y;
+  constructor(x,y, theImage){
+    this.x = x + 35;
+    this.y = y + 20;
     this.dx = 5;
-    this.diameter = 8;
+    this.w = 8;
+    this.h = 8;
     this.colour = "black";
+    this.image = theImage;
   }
   display(){
-    fill(this.colour);
-    stroke(this.colour);
-    circle(this.x, this.y, this.diameter);
+
+    image(this.image, this.x, this.y, this.w, this.h);
   }
   move(){
     this.x += this.dx;
   }
   isDead(){
-    return this.x > 2000;
+    return this.x > width;
   }
 }
 
 let infiniteBullets = [];
 
-let playerX = 100;
-let playerY = 100;
+let playerX = 65;
+let playerY = 85;
+let megaman;
+let plasmabullet;
+
+function preload(){
+  megaman = loadImage("megaman.png");
+  plasmabullet = loadImage("plasmabullet.png");
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  let someBullet = new Bullet(playerX, playerY);
+  let someBullet = new Bullet(playerX, playerY, plasmabullet);
   infiniteBullets.push(someBullet);
 }
 
 function draw() {
-  background(220);
+  background(255);
   fill("red");
-  rect(playerX, playerY, 10);
+  image(megaman, playerX, playerY, megaman.width * 0.25, megaman.height *0.25);
   for (let i = 0; i < infiniteBullets.length; i++){
-    if(infiniteBullets[i].isDead){
-      infiniteBullets[i].splice(i,1);
+    if(infiniteBullets[i].isDead()){
+      infiniteBullets.splice(i,1);
     }
     else{
       infiniteBullets[i].display();
@@ -54,6 +62,6 @@ function draw() {
 }
 
 function mousePressed() {
-  let someBullet = new Bullet(playerX, playerY);
+  let someBullet = new Bullet(playerX, playerY, plasmabullet);
   infiniteBullets.push(someBullet);
 }
